ac77e18db3ea0cabffa67bf4aec4df14180b3d50
Romil Tickoo <romil.tickoo@echelonsg.com>
2021-11-01 06:55:41 +0000

    first Commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("search.po.firstclass.controller.View1", {
+			onInit: function () {
+                 
+
+            }
+        
+               
+            
+		});
+	});
